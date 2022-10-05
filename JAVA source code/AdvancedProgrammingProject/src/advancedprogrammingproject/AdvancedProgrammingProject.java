3ab2e53f38bfbe112b363ec462cee39ae40ca8ae
abulaziz bahamid <gimo999@hotmail.com>
2022-10-05 23:34:41 +0300

    connecting to online hosted database

--- JAVA source code/AdvancedProgrammingProject/src/advancedprogrammingproject/AdvancedProgrammingProject.java
+++ JAVA source code/AdvancedProgrammingProject/src/advancedprogrammingproject/AdvancedProgrammingProject.java
@@ -14,40 +14,41 @@ public class AdvancedProgrammingProject {
 //        DriverManager.registerDriver(md);
 
         // THIS PART RETRIEVES DATA FROM DBMS-----------------------------------------
-        String mysqlUrl = "jdbc:mysql://localhost/StudentsDB_Bahamid_AlAmoudi";
-        Connection con = DriverManager.getConnection(mysqlUrl, "root", "aziz099");
+        String mysqlUrl = "jdbc:mysql://bdwyaxuoiy38wddwtdxs-mysql.services.clever-cloud.com/bdwyaxuoiy38wddwtdxs";
+        Connection con = DriverManager.getConnection(mysqlUrl, "uewldxfzpbauu3nf", "vTJ5cQWE1Wv9aRo0z0ZT");
         System.out.println("Connection established: " + con);
-        
-        String selectqu = "select * from StudentsTBL_abdulaziz_abdulaziz";
+
+        String selectqu = "select * from WORD";
         PreparedStatement sst = con.prepareStatement(selectqu);
 
         ResultSet rst = sst.executeQuery();
-        
+
         while (rst.next()) {
             System.out.println(rst.getInt(1));
             System.out.println(rst.getString(2));
-            System.out.println(rst.getDate(3));
-            System.out.println(rst.getDouble(4));
+            System.out.println(rst.getString(3));
+//            System.out.println(rst.getDate(3));
+//            System.out.println(rst.getDouble(4));
         }
         //THIS PART INSERTS DATA INTO TABLE -------------------------------
-        String insertqu = "insert into StudentsTBL_abdulaziz_abdulaziz(FullName,DateOfBirth,GPA) values(?,?,?)";
-        PreparedStatement ist = con.prepareStatement(insertqu);
-        ist.setString(1, "ahmmmm");
-        ist.setDate(2, java.sql.Date.valueOf("2020-09-04"));
-        ist.setDouble(3, 3.4);
-        ist.execute();
+//        String insertqu = "insert into StudentsTBL_abdulaziz_abdulaziz(FullName,DateOfBirth,GPA) values(?,?,?)";
+//        PreparedStatement ist = con.prepareStatement(insertqu);
+//        ist.setString(1, "ahmed");
+//        ist.setDate(2, java.sql.Date.valueOf("2020-09-04"));
+//        ist.setDouble(3, 3.4);
+//        ist.execute();
         //THIS PART SEARCHES FOR ALL MATCHING RESULTS IN DATABASE BASED ON FULLNAME-------------
-        System.out.println("------------------------------------------");
-        String searchqu = "select * from StudentsTBL_abdulaziz_abdulaziz where fullName = ?";
-        PreparedStatement qst = con.prepareStatement(searchqu);
-        qst.setString(1, "aziz");
-        ResultSet searchResult = qst.executeQuery();
-        while (searchResult.next()) {
-            System.out.println(searchResult.getInt(1));
-            System.out.println(searchResult.getString(2));
-            System.out.println(searchResult.getDate(3));
-            System.out.println(searchResult.getDouble(4));
-        }
+//        System.out.println("------------------------------------------");
+//        String searchqu = "select * from StudentsTBL_abdulaziz_abdulaziz where fullName = ?";
+//        PreparedStatement qst = con.prepareStatement(searchqu);
+//        qst.setString(1, "aziz");
+//        ResultSet searchResult = qst.executeQuery();
+//        while (searchResult.next()) {
+//            System.out.println(searchResult.getInt(1));
+//            System.out.println(searchResult.getString(2));
+//            System.out.println(searchResult.getDate(3));
+//            System.out.println(searchResult.getDouble(4));
+//        }
         //--------------------------------------       
         System.out.println("Project Done By: Abdulaziz Alamoudi + Abdulaziz Bahamid");
         Scanner input = new Scanner(System.in);
