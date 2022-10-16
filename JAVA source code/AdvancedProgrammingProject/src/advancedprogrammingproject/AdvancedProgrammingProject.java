616f62a41609145058e8129539b11c1a594519be
abulaziz bahamid <gimo999@hotmail.com>
2022-10-16 20:42:12 +0300

    added some info to the report

--- JAVA source code/AdvancedProgrammingProject/src/advancedprogrammingproject/AdvancedProgrammingProject.java
+++ JAVA source code/AdvancedProgrammingProject/src/advancedprogrammingproject/AdvancedProgrammingProject.java
@@ -12,10 +12,10 @@ public class AdvancedProgrammingProject {
 
     public static void main(String[] args) throws SQLException, IOException {
         // Welcoming message to program
-        System.out.println("\t\t\t\t{ Welcome to Students DBMS }");
-        System.out.println("\t\t\t\t\tProject Done By:");
-        System.out.println("\t\t\t\t--Abdulaziz Alamoudi 441016500");
-        System.out.println("\t\t\t\t--Abdulaziz Bahamid  441016576");
+        System.out.println("\t\t{ Welcome to Students DBMS }");
+        System.out.println("\t\t\tProject Done By:");
+        System.out.println("\t\t--Abdulaziz Alamoudi 441016500");
+        System.out.println("\t\t--Abdulaziz Bahamid  441016576");
         Scanner input = new Scanner(System.in);
         String choice;
         
@@ -25,7 +25,7 @@ public class AdvancedProgrammingProject {
             try{
             switch (choice) { 
                 case "1":
-                    String name = DBUtility.readName("Enter full name ---> ");
+                    String name = DBUtility.readName("\t\tEnter full name ---> ");
                     String date = DBUtility.readDate();
                     double GPA = DBUtility.readGPA();
                     DBUtility.addRecord(name, date, GPA);
@@ -36,32 +36,32 @@ public class AdvancedProgrammingProject {
                     break;
                     
                 case "3":
-                    String fullname = DBUtility.readName("Enter a name to search ---> ");
+                    String fullname = DBUtility.readName("\t\tEnter a name to search ---> ");
                     DBUtility.searchByName(fullname);
                     break;
                     
                 case "4":
-                    System.out.println(" -- Thanks for using our Students DBMS -- ");
+                    System.out.println("\t\t -- Thanks for using our Students DBMS -- ");
                     break;
                     
                 default:
-                    System.out.println("Sorry Wrong input !!");
-                    System.out.println("Try Again");
+                    System.out.println("\t\tSorry Wrong input !!");
+                    System.out.println("\t\tTry Again");
                     }
             } catch(Exception Error){
-                    System.out.println("Something went wrong please try again!!");
-                    System.out.println("the ERROR was: " + Error);
+                    System.out.println("\t\tSomething went wrong please try again!!");
+                    System.out.println("\t\tthe ERROR was: " + Error);
             }// end of swtich case
         } while (!choice.equals("4"));
     }// end of main method
 
     public static void printMenu() {
         System.out.println("\n");
-        System.out.println("Please choose from the menu below:");
-        System.out.println("1. add a student");
-        System.out.println("2. Read All Student Records");
-        System.out.println("3. Search For a Student By Name");
-        System.out.println("4. Exit");
-        System.out.print("Your Option: ");
+        System.out.println("\t\tPlease choose from the menu below:");
+        System.out.println("\t\t1. add a student");
+        System.out.println("\t\t2. Read All Student Records");
+        System.out.println("\t\t3. Search For a Student By Name");
+        System.out.println("\t\t4. Exit");
+        System.out.print("\t\tYour Option: ");
     }// end of printMenu Method
 }//end of class
--- JAVA source code/AdvancedProgrammingProject/src/advancedprogrammingproject/DBUtility.java
+++ JAVA source code/AdvancedProgrammingProject/src/advancedprogrammingproject/DBUtility.java
@@ -21,10 +21,11 @@ public class DBUtility {
     static Scanner input = new Scanner(System.in);
     
     public static Connection getConnection() throws SQLException, FileNotFoundException, IOException {
+        // grapping the configuration file to extract our secret information like the database url, the user and his password
         Properties config=new Properties();
         FileInputStream ip= new FileInputStream("config.properties");
         config.load(ip);
-        
+        //concecting to the database then retrun connection object
         return DriverManager.getConnection(config.getProperty("DBUrl"), config.getProperty("user"), config.getProperty("password"));
     }// end of getConnection method
 
@@ -38,7 +39,7 @@ public class DBUtility {
         statement.setDate(2, java.sql.Date.valueOf(date));
         statement.setDouble(3, GPA);
         statement.execute();
-        System.out.println("\t++info added successfully++\n");
+        System.out.println("\t\t\t++info added successfully++\n");
         
         // closing the opened connections
         statement.close();
@@ -63,7 +64,7 @@ public class DBUtility {
             }
             System.out.println("--------------------------------------------------------------------------");
         } else {//did not fount any match in the data exception
-            System.out.println("There are NO records in the database !!");
+            System.out.println("\t\tThere are NO records in the database !!");
         }
         
         // closing the opened connections
@@ -91,7 +92,7 @@ public class DBUtility {
             }
             System.out.println("--------------------------------------------------------------------------");
         } else {
-            System.out.println("No Records found for the name '" + fullname + "'");
+            System.out.println("\t\tNo Records found for the name '" + fullname + "'");
         }
         
         // closing the opened connections
@@ -107,9 +108,9 @@ public class DBUtility {
         
         if (!name.matches(Name_Pattern)) {//matching the input with the regular expression
             if (name.length() > 40 || name.length() < 3) {//checks what error the user madde
-                System.out.println("your name should be 3 to 40 characters !!");
+                System.out.println("\t\tyour name should be 3 to 40 characters !!");
             } else {
-                System.out.println("please just enter characters not numbers !!");
+                System.out.println("\t\tplease just enter characters not numbers !!");
             }
             name = readName(text); // keeps recalling the same method till receive a valid input
         }
@@ -117,7 +118,7 @@ public class DBUtility {
     }// end of readName method
 
     public static String readDate() {
-        System.out.print("Enter birth date in yyyy-mm-dd form ---> ");
+        System.out.print("\t\tEnter birth date in yyyy-mm-dd form ---> ");
         String date = input.nextLine();
         // constructin the regular expression to check the format and valditiy of the input with it
         String DATE_PATTERN = "((18|19|20)[0-9]{2}[-](0?[13578]|1[02])[-](0?[1-9]|[12][0-9]|3[01]))|"
@@ -128,13 +129,12 @@ public class DBUtility {
         if (!date.matches(DATE_PATTERN)) { //matching the input with the regular expression
             String dateFormat = "\\d{4}-(\\d{2}|\\d)-(\\d{2}|\\d)";
             if (!date.matches(dateFormat)) { //checks what error the user madde
-                System.out.println("please enter date in the yyyy-mm-dd form !!");
+                System.out.println("\t\tplease enter date in the yyyy-mm-dd form !!");
             } else {
-                System.out.println("please enter a valid date !!");
+                System.out.println("\t\tplease enter a valid date !!");
             }
             date = readDate(); // keeps recalling the same method till receive a valid input
         }
-
         return date;
     }// end of readDate method
 
@@ -142,14 +142,14 @@ public class DBUtility {
         //we take the user input as double
         double GPA;
         try{
-            System.out.print("Enter student GPA \"must be between 0.00 to 4.00\" ---> ");
+            System.out.print("\t\tEnter student GPA \"must be between 0.00 to 4.00\" ---> ");
             GPA = input.nextDouble();
         } catch(Exception e){// catch if the user entred something other than number
-            System.out.println("the input you entred wasn't a number!!");
+            System.out.println("\t\tthe input you entred wasn't a number!!");
             GPA = readGPA();// keeps recalling the same method till receive a valid input
         }
         if (GPA < 0 | GPA > 4) { // check if the entred GPA is invalid
-            System.out.println("please enter a GPA between 0.00 to 4.00 !!");
+            System.out.println("\t\tplease enter a GPA between 0.00 to 4.00 !!");
             GPA = readGPA(); // keeps recalling the same method till receive a valid input
         }
         return GPA;
